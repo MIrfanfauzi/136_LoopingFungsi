5da37ee573557d06669bbd0621c0929674dea4a5
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-03-18 12:19:40 +0700

    .

--- Prak3/Prak3.cpp
+++ Prak3/Prak3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        cout << i << ". " << "Nama saya Irfan" << endl;
+
+    }
+
+    string arr[5];
+
+    for (i = 0; i > 5; i--) {
+        cout << "Masukan nickname ke " << i + 1 << " = ";
+        cin >> arr[i];
+    }
+
+    for (i = 0; i < 5; i++) {
+        cout << "Namanya adalah = " << arr[i] << endl;
+    }
+
+
+    return 0;
+
+}
